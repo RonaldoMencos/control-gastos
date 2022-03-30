130bcdf10efe117cad98993ad014fa33f6392c0a
Oscar Mencos <oscaronaldo2001@gmail.com>
2022-03-30 15:29:14 -0600

    Añadiendo Modal y Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,53 @@
+import { useState } from "react"
+import Header from "./components/Header"
+import Modal from "./components/Modal";
+import IconoNuevoGasto from './img/nuevo-gasto.svg';
+
+
 function App() {
+  const [presupuesto, setpresupuesto] = useState(0)
+  const [isvalid, setisvalid] = useState(false)
+
+  const [modal, setmodal] = useState(false)
+  const [animarModal, setanimarModal] = useState(false)
+
+
+  const handleNuevoGasto = () => {
+    setmodal(true)
+
+    setTimeout(() => {
+      setanimarModal(true)
+    }, 1000);
+  }
 
   return (
-    <h1>Control de gastos</h1>
+    
+    <div>
+      <Header 
+        presupuesto={presupuesto}
+        setpresupuesto={setpresupuesto}
+        isvalid={isvalid}
+        setisvalid={setisvalid}
+      />
+      {isvalid && (
+        <div className="nuevo-gasto">
+        <img 
+          src={IconoNuevoGasto} 
+          alt="icono nuevo gasto" 
+          onClick={handleNuevoGasto}  
+        />
+      </div>
+      ) 
+      }
+
+      {modal && (
+      <Modal 
+        setmodal={setmodal}
+        animarModal={animarModal}
+      />
+      )}
+      
+    </div>
   )
 }
 
